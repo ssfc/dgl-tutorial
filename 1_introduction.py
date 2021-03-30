f43b9f7ce3f3a9c501194007a9f40be2bc73282d
YE GAO <sortsimilar@gmail.com>
2021-03-30 19:09:57 +0800

    Update 1_introduction.py

--- 1_introduction.py
+++ 1_introduction.py
@@ -148,8 +148,8 @@ print(g.edata)
 class GCN(nn.Module):
     def __init__(self, in_feats, h_feats, num_classes):
         super(GCN, self).__init__()
-        self.conv1 = GraphConv(in_feats, h_feats)
-        self.conv2 = GraphConv(h_feats, num_classes)
+        self.conv1 = GraphConv(in_feats, h_feats)  # input features, hidden features; 
+        self.conv2 = GraphConv(h_feats, num_classes)  # hidden features, number of class; 
     
     def forward(self, g, in_feat):
         h = self.conv1(g, in_feat)
