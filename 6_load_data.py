54fe8ade47c3808b42a3351269e00ae862716acb
YE GAO <sortsimilar@gmail.com>
2021-05-08 16:24:49 +0800

    Update 6_load_data.py

--- 6_load_data.py
+++ 6_load_data.py
@@ -111,14 +111,15 @@ class KarateClubDataset(DGLDataset):
         edges_dst = torch.from_numpy(edges_data['Dst'].to_numpy())
         
         self.graph = dgl.graph((edges_src, edges_dst), num_nodes=nodes_data.shape[0])
-        self.graph.ndata['feat'] = node_features
-        self.graph.ndata['label'] = node_labels
-        self.graph.edata['weight'] = edge_features
+        self.graph.ndata['feat'] = node_features  # tensors; 
+        self.graph.ndata['label'] = node_labels  # tensors; 
+        self.graph.edata['weight'] = edge_features  # tensors; 
         
         # If your dataset is a node classification dataset, you will need to assign
         # masks indicating whether a node belongs to training, validation, and test set.
         n_nodes = nodes_data.shape[0]
         n_train = int(n_nodes * 0.6)
+
         n_val = int(n_nodes * 0.2)
         train_mask = torch.zeros(n_nodes, dtype=torch.bool)
         val_mask = torch.zeros(n_nodes, dtype=torch.bool)
